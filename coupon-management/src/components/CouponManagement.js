0e4f9ec41dc7824d810901b499816fd827209d3b
Yehuda Volodin <falcon81@tut.by>
2021-04-12 14:51:13 +0300

    Coupons Table was added.

--- coupon-management/src/components/CouponManagement.js
+++ coupon-management/src/components/CouponManagement.js
@@ -5,20 +5,50 @@ import './CouponManagement.css';
 import MainHeader from './MainHeader';
 
 export default function CouponManagement() {
+    const [coupons, setCoupons] = useState([
+        { brandName: "Starbucks", couponTitle: "Free cupcake", coupons: "123/278", issued: 98, used: 57, status: true },
+        { brandName: "McDonald’s", couponTitle: "30% meal discount", coupons: "123/278", issued: 274, used: 57, status: true },
+        { brandName: "Children land", couponTitle: "1+1 Tickets", coupons: "123/278", issued: 0, used: 57, status: false },
+        { brandName: "McDonald’s", couponTitle: "Free ice-cream", coupons: "123/278", issued: 98, used: 57, status: false }
+    ]);
+
+    const couponsTable = coupons.map((element, index) =>
+        <div style={{
+            "paddingTop": "51px",
+            "paddingBottom": "53px",
+            "height": "24px",
+            "borderBottom": "1px solid #E3E3E3",
+
+            "paddingLeft": "121px",
+            "fontSize": "18px",
+            "fontWeight": "600",
+            "lineHeight": "24px",
+            "letterSpacing": "0.3px"
+        }}>
+            {element.brandName}
+            <span style={{ "marginLeft": "78px" }}> {element.couponTitle} </span>
+            <span style={{ "marginLeft": "105.5px" }}> {element.coupons} </span>
+            <span style={{ "marginLeft": "88.5px" }}> {element.issued} </span>
+            <span style={{ "marginLeft": "72.5px" }}> {element.used} </span>
+            <span style={{ "marginLeft": "84.5px" }}> {element.status ? "Active" : "Inactive"} </span>
+        </div>
+    )
+
     return (
         <div className="mainDiv">
             <MainHeader
                 headerText="Coupon Management"
-                headerStyle={{ "fontSize":"33.32px", "float":"left", "letterSpacing": "-0.01px" }}
+                headerStyle={{ "fontSize": "33.32px", "float": "left", "letterSpacing": "-0.01px" }}
             />
             {/*Coupons table headers*/}
             <div style={{
                 "marginTop": "131px",
-                "paddingLeft": "121px",
                 "paddingTop": "19.5px",
                 "paddingBottom": "18.5px",
                 "backgroundColor": "#F6F7F8",
                 "height": "24px",
+
+                "paddingLeft": "121px",
                 "fontSize": "18px",
                 "fontWeight": "600",
                 "lineHeight": "24px",
@@ -31,6 +61,8 @@ export default function CouponManagement() {
                 <span style={{ "marginLeft": "72.5px" }}> Used </span>
                 <span style={{ "marginLeft": "84.5px" }}> Status </span>
             </div>
+
+            {couponsTable}
         </div>
     )
 }
