c24ca8abaaf13693d2d33744300d368f85628fc9
Yehuda Volodin <falcon81@tut.by>
2021-04-13 14:35:55 +0300

    Coupons Table was moved to new separate component <CouponList>

--- coupon-management/src/components/CouponManagement.js
+++ coupon-management/src/components/CouponManagement.js
@@ -3,8 +3,7 @@ import React, { useState } from 'react'
 import './CouponManagement.css';
 
 import MainHeader from './MainHeader';
-
-import rightArrow from './rightArrow.svg';
+import CouponList from './CouponList';
 
 export default function CouponManagement() {
     const [coupons, setCoupons] = useState([
@@ -13,47 +12,6 @@ export default function CouponManagement() {
         { brandName: "Children land", couponTitle: "1+1 Tickets", coupons: "123/278", issued: 0, used: 57, status: false },
         { brandName: "McDonald’s", couponTitle: "Free ice-cream", coupons: "123/278", issued: 98, used: 57, status: false }
     ]);
-
-    const couponsTable = coupons.map((element, index) =>
-        <div style={{
-            "paddingTop": "51px",
-            "paddingBottom": "53px",
-            "height": "24px",
-            "position":"relative",
-
-            "paddingLeft": "121.5px",
-            "fontSize": "18px",
-            "fontWeight": "600",
-            "lineHeight": "24px",
-            "letterSpacing": "0.3px"
-        }}>
-            <span style={{ "display": "inline-block", "minWidth": "121px" }}> {element.brandName} </span>
-            <span style={{ "marginLeft": "69px", "display": "inline-block", "minWidth": "179px" }}> {element.couponTitle} </span>
-            <span style={{ "marginLeft": "45px" }}>
-                {element.coupons.split('/', 2)[0] + "/"}
-                <span style={{ "fontWeight": "normal" }}>{element.coupons.split('/', 2)[1]}</span>
-            </span>
-            {element.issued > 0 ?
-                <span style={{ "marginLeft":"100px", "display":"inline-block", "minWidth":"42px", "fontWeight":"bold", "color":"#6744D7" }}> {element.issued} </span>
-                :
-                <span style={{ "marginLeft":"100px", "display":"inline-block", "minWidth":"42px", "fontWeight":"bold", "color":"#FF3158" }}> {element.issued} </span>
-            }
-            <span style={{ "marginLeft":"90px", "fontWeight":"bold", "color":"#FF3158" }}> {element.used} </span>
-            {element.status ?
-                <span style={{ "marginLeft":"105px", "display":"inline-block", "minWidth":"71px", "fontWeight":"bold", "color":"#44D7B6" }}> Active </span>
-                :
-                <span style={{ "marginLeft":"105px", "display":"inline-block", "minWidth":"71px", "fontWeight":"bold", "color":"#FF5575" }}> Inactive </span>
-            }
-            <img
-                style={{ "marginLeft": "75px" }}
-                src={rightArrow}
-                alt="Right Arrow"
-                width="14"
-                height="24"
-            />
-            <div style={{ "borderBottom":"1px solid #E3E3E3", "width": "1108px", "position":"absolute","bottom":"0", "left":"47.5px" }}/>
-        </div>
-    )
 
     return (
         <div className="mainDiv">
@@ -84,8 +42,7 @@ export default function CouponManagement() {
                 <span style={{ "marginLeft": "72.5px" }}> Used </span>
                 <span style={{ "marginLeft": "84.5px" }}> Status </span>
             </div>
-
-            {couponsTable}
+            <CouponList coupons={coupons} />
         </div>
     )
 }
